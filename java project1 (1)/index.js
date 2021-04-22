829b153bee3b7b6e278468fd96aaee8e8332672d
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-04-22 15:06:26 +0530

    Add files via upload

---
+++ java project1 (1)/index.js
@@ -0,0 +1,11 @@
+function handleSubmit () {
+    const name = document.getElementById('username').value;
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname); */
+
+    sessionStorage.setItem("NAME", name);
+
+    return;
+}
